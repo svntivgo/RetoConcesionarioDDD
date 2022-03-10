0ad659e8e425f36ca47869003b1c4a0f2c12cbf1
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-09 22:52:41 -0500

    creacion entidad carro

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+|
+Lsrc/main/java/co/com/sofka/sofkacars/domain/concesionario/Concesionario.java,c\0\c0d99f4e1c179708836963d1df1e44ab9d6f53d9
+Ç
+Rsrc/main/java/co/com/sofka/sofkacars/domain/concesionario/ids/AdministradorId.java,e\b\ebc5380b6bd6d7fa3ba8f71d1432c94a1e0147ef
+Å
+Qsrc/main/java/co/com/sofka/sofkacars/domain/concesionario/valueobjects/Marca.java,1\4\1479e217bba2e34786967604efbb669e88efdd0c
+{
+Ksrc/main/java/co/com/sofka/sofkacars/domain/concesionario/ids/AsesorId.java,5\5\558feeb64a541cea39556cbd89c7ddbb3796f672
+z
+Jsrc/main/java/co/com/sofka/sofkacars/domain/concesionario/ids/CarroId.java,5\a\5ac2b567ce72a0de78ea852e203af6c71de9f096
+Å
+Qsrc/main/java/co/com/sofka/sofkacars/domain/concesionario/valueobjects/Email.java,c\0\c0f77f8f71d7e6c935ee3d59dd00f961c446f888
+Å
+Qsrc/main/java/co/com/sofka/sofkacars/domain/concesionario/valueobjects/Color.java,1\1\110a155b3f380e060f061114bec99ccb0a4032ee
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/co/com/sofka/sofkacars/domain/concesionario/entities/Carro.java
+++ src/main/java/co/com/sofka/sofkacars/domain/concesionario/entities/Carro.java
@@ -1,4 +1,40 @@
 package co.com.sofka.sofkacars.domain.concesionario.entities;
 
-public class Carro {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.sofkacars.domain.concesionario.ids.CarroId;
+import co.com.sofka.sofkacars.domain.concesionario.valueobjects.Color;
+import co.com.sofka.sofkacars.domain.concesionario.valueobjects.Marca;
+import co.com.sofka.sofkacars.domain.concesionario.valueobjects.Modelo;
+import co.com.sofka.sofkacars.generics.Precio;
+
+import java.util.Objects;
+
+public class Carro extends Entity<CarroId> {
+
+    private Marca marca;
+    private Modelo modelo;
+    private Color color;
+    private Precio precio;
+
+    // se crea un constructor publico para los objectValue
+
+    public Carro(CarroId carroId, Marca marca, Modelo modelo, Color color, Precio precio) {
+        // Super --> herencia
+        super(carroId);
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.precio = precio;
+    }
+
+    // métodos de la entidad Asesor
+
+    public void cambiarMarca (Marca marca) {this.marca = Objects.requireNonNull(marca);}
+
+    public void cambiarModelo (Modelo modelo) {this.modelo = Objects.requireNonNull(modelo);}
+
+    public void cambiarColor (Color color) {this.color = Objects.requireNonNull(color);}
+
+    public void cambiarPrecio (Precio precio) {this.precio = Objects.requireNonNull(precio);}
+
 }
