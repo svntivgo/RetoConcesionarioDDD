211dd65f2e9fa8fe5c3b3c49235b0112a77a5a1e
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-11 00:09:21 -0500

    se crea evento cliente

---
+++ src/main/java/co/com/sofka/sofkacars/domain/factura/events/ClienteCambiado.java
@@ -0,0 +1,41 @@
+package co.com.sofka.sofkacars.domain.factura.events;
+
+import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofka.sofkacars.domain.factura.ids.ClienteId;
+import co.com.sofka.sofkacars.generics.Identificacion;
+import co.com.sofka.sofkacars.generics.Nombre;
+import co.com.sofka.sofkacars.generics.Telefono;
+
+import java.util.Set;
+
+public class ClienteCambiado extends DomainEvent {
+
+    private final ClienteId clienteId;
+    private final Nombre nombre;
+    private final Set<Telefono> telefono;
+    private final Identificacion identificacion;
+
+    public ClienteCambiado(ClienteId clienteId, Nombre nombre, Set<Telefono> telefono, Identificacion identificacion) {
+        super("RopaDeportiva.ClienteCambiado");
+        this.clienteId = clienteId;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.identificacion = identificacion;
+    }
+
+    public ClienteId getClienteId() {
+        return clienteId;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public Set<Telefono> getTelefono() {
+        return telefono;
+    }
+
+    public Identificacion getIdentificacion() {
+        return identificacion;
+    }
+}
